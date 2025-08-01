b5e6fa294d1ece2c638adccd7527712200625a6e
Nishank018 <gnishank8076@gmail.com>
2025-08-01 17:22:44 +0530

    Initial working version of Rock Paper Scissors

--- practice/script.js
+++ practice/script.js
@@ -1,5 +1,35 @@
 
-let container = document.querySelector("#contianer")
+let btn = document.querySelector("#btn")
+// let container = document.querySelector("#container")
+
+// let content = document.createElement("div")
+// content.classList.add("content")
+// content.textContent = "This is gloriouas text-content!"
+// content.setAttribute("style", "background: pink; color: blue; padding: 10px; border: 2px solid black")
+
+
+// let para = document.createElement("p")
+// para.classList.add("para")
+// para.textContent = "Hey I'm red!"
+
+// let div = document.createElement("div")
+
+
+
+btn.addEventListener("click", function() {
+  alert("Button was clicked!");
+});
+
+
+
+
+
+
+
+
+//  container.appendChild(para)
+//  container.appendChild(content)
+// container.appendChild(div)
 
 
 
